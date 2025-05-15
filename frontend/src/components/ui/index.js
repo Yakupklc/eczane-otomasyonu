6d60847f583199bb2585a2b3061f9cab094d51a1
Yakupklc <yakupkilic876@gmail.com>
2025-05-15 22:44:51 +0300

    index.js dosyasında boşluk değişiklikleri düzeltildi

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -1,9 +1,9 @@
 import AppHeader from './AppHeader';
 import SideMenu from './SideMenu';
 import ContentWrapper from './ContentWrapper';
-
+ 
 export {
   AppHeader,
   SideMenu,
   ContentWrapper
-}; 
+};
